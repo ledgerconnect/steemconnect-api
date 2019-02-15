50acdaa25d245c8546725781d212a4e2cdda3d14
adcpm <fabien@bonustrack.co>
2019-02-15 21:34:29 +0700

    Remove app creation, edition and secret reset

--- routes/apps.js
+++ routes/apps.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const crypto = require('crypto');
-const has = require('lodash/has');
 const { authenticate } = require('../helpers/middleware');
-const config = require('../config.json');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
@@ -30,103 +27,6 @@ router.get('/@:clientId', async (req, res, next) => {
       app.allowed_ips = undefined;
     }
     res.json(app);
-  }
-});
-
-/** Create application */
-router.post('/@:clientId', authenticate('user'), async (req, res) => {
-  const { clientId } = req.params;
-
-  const accounts = await req.steem.api.getAccountsAsync([clientId]);
-  if (!accounts[0]) {
-    res.status(400).json({ error: `Proxy account @${clientId} does not exist` });
-  } else {
-    const proxy = accounts[0];
-    const proxyAuthStr = JSON.stringify({
-      owner: proxy.owner,
-      active: proxy.active,
-      posting: proxy.posting,
-      memo: proxy.memo_key,
-    });
-    const offlinePubKeys = config.offline_generated_public_keys;
-    const requiredAuthStr = JSON.stringify({
-      owner: { weight_threshold: 1, account_auths: [['steemconnect', 1]], key_auths: [[offlinePubKeys.owner, 1]] },
-      active: { weight_threshold: 1, account_auths: [['steemconnect', 1]], key_auths: [[offlinePubKeys.active, 1]] },
-      posting: { weight_threshold: 1, account_auths: [['steemconnect', 1]], key_auths: [[offlinePubKeys.posting, 1]] },
-      memo: offlinePubKeys.memo,
-    });
-
-    let jsonMetadata;
-    try {
-      jsonMetadata = JSON.parse(proxy.json_metadata);
-    } catch (e) {
-      jsonMetadata = {};
-    }
-
-    if (
-      proxyAuthStr === requiredAuthStr
-      && jsonMetadata.owner && jsonMetadata.owner === req.user
-    ) {
-      const secret = crypto.randomBytes(24).toString('hex');
-      req.db.apps.create({
-        client_id: clientId,
-        secret,
-        owner: req.user,
-      }).then(() => {
-        res.json({ success: true });
-      }).catch((err) => {
-        const error = has(err, 'original.detail')
-          ? err.original.detail
-          : err;
-        res.status(400).json({ error });
-      });
-    } else {
-      res.status(400).json({ error: `Proxy account @${clientId} is not valid` });
-    }
-  }
-});
-
-/** Update application */
-router.put('/@:clientId', authenticate('user'), async (req, res, next) => {
-  const { clientId } = req.params;
-  const app = req.body;
-  try {
-    await req.db.apps.update({
-      redirect_uris: app.redirect_uris,
-      name: app.name,
-      description: app.description,
-      icon: app.icon,
-      website: app.website,
-      is_public: app.is_public,
-      allowed_ips: app.allowed_ips,
-    }, {
-      where: {
-        client_id: clientId,
-        owner: req.user,
-      },
-    });
-    res.json({ success: true });
-  } catch (err) {
-    next(err);
-  }
-});
-
-/** Reset client secret */
-router.put('/@:clientId/reset-secret', authenticate('user'), async (req, res, next) => {
-  const { clientId } = req.params;
-  const secret = crypto.randomBytes(24).toString('hex');
-  try {
-    await req.db.apps.update({
-      secret,
-    }, {
-      where: {
-        client_id: clientId,
-        owner: req.user,
-      },
-    });
-    res.json({ success: true, secret });
-  } catch (err) {
-    next(err);
   }
 });
 

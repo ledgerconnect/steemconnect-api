92b804ddd248745a0fbd7a9eaa313d7dbedcdecc
adcpm <fabien@bonustrack.co>
2019-02-27 06:29:22 +0700

    Clean

--- routes/api.js
+++ routes/api.js
@@ -68,7 +68,6 @@ router.put('/me', authenticate('app'), async (req, res) => {
     error: 'invalid_request',
     error_description: 'User metadata must be an object',
   });
-  return;
 });
 
 /** Get my account details */
@@ -85,8 +84,8 @@ router.all('/me', authenticate(), async (req, res) => {
   let userMetadata;
   try {
     userMetadata = req.role === 'app'
-    ? await getUserMetadata(req.proxy, req.user)
-    : undefined;
+      ? await getUserMetadata(req.proxy, req.user)
+      : undefined;
   } catch (err) {
     console.error(`Get user metadata of @${req.user} failed`, err);
     res.status(501).send('request failed');
@@ -100,7 +99,6 @@ router.all('/me', authenticate(), async (req, res) => {
     scope,
     user_metadata: userMetadata,
   });
-  return;
 });
 
 /** Broadcast transaction */
@@ -134,7 +132,6 @@ router.post('/broadcast', authenticate('app'), verifyPermissions, async (req, re
       error_description: `This access_token allow you to broadcast transaction only for the account @${req.user}`,
     });
   } else {
-
     /** Store global broadcast count per month and by app */
     const month = new Date().getUTCMonth() + 1;
     const year = new Date().getUTCFullYear();
@@ -190,7 +187,7 @@ router.post('/broadcast', authenticate('app'), verifyPermissions, async (req, re
 });
 
 router.all('/login/challenge', async (req, res) => {
-  const username = req.query.username;
+  const { username } = req.query;
   const role = ['posting', 'active', 'owner'].includes(req.query.role) ? req.query.role : 'posting';
   const token = issueUserToken(username);
   let accounts;
@@ -202,14 +199,11 @@ router.all('/login/challenge', async (req, res) => {
     return;
   }
   const keyAuths = accounts[0][role].key_auths;
-  const codes = keyAuths.map(keyAuth =>
-    encode(process.env.BROADCASTER_POSTING_WIF, keyAuth[0], `#${token}`)
-  );
+  const codes = keyAuths.map(keyAuth => encode(process.env.BROADCASTER_POSTING_WIF, keyAuth[0], `#${token}`));
   res.json({
     username,
     codes,
   });
-  return;
 });
 
 /**
@@ -234,8 +228,8 @@ router.all('/token/revoke/:type/:clientId?', authenticate('user'), async (req, r
   }
 
   if (
-    (type === 'user' && (where.user || where.client_id)) ||
-    (type === 'app' && where.client_id)
+    (type === 'user' && (where.user || where.client_id))
+    || (type === 'app' && where.client_id)
   ) {
     await tokens.destroy({ where });
   }
--- start.js
+++ start.js
@@ -1,42 +1,30 @@
-/* eslint-disable no-param-reassign,new-cap */
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const http = require('http');
-const https = require('https');
 const cors = require('cors');
 const steem = require('@steemit/steem-js');
 const { strategy } = require('./helpers/middleware');
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 if (process.env.STEEMD_URL) {
   steem.api.setOptions({ url: process.env.STEEMD_URL });
 }
 
-http.globalAgent.maxSockets = Infinity;
-https.globalAgent.maxSockets = Infinity;
-const app = express();
-const server = http.Server(app);
-
-// iframe header
 app.use((req, res, next) => {
   res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  req.steem = steem;
   next();
 });
 
 app.enable('trust proxy');
 app.disable('x-powered-by');
 
-app.use((req, res, next) => {
-  req.steem = steem;
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-
 app.use(strategy);
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', require('./routes/api'));
@@ -46,22 +34,6 @@ app.get('/*', (req, res) => {
   res.redirect(`https://app.steemconnect.com${req.url}`);
 });
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
-
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : err;
-
-  // render the error page
-  res.status(err.status || 500);
-  res.json(err);
-});
-
-module.exports = { app, server };

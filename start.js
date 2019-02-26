bbeca418056fef282139c079e9dd43e5c83481c1
adcpm <fabien@bonustrack.co>
2019-02-27 06:43:21 +0700

    Fix body-parser limit

--- start.js
+++ start.js
@@ -21,8 +21,8 @@ app.use((req, res, next) => {
 app.enable('trust proxy');
 app.disable('x-powered-by');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '20mb' }));
+app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
 app.use(cors());
 app.use(strategy);
 app.use(express.static(path.join(__dirname, 'public')));

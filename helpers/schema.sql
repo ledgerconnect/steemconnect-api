b2399ebe2a090a2872685728b711d35c0c53d266
adcpm <fabien@bonustrack.co>
2019-03-16 19:38:21 +0700

    Update SQL schema

--- helpers/schema.sql
+++ helpers/schema.sql
@@ -4,15 +4,16 @@ CREATE TABLE token (
   username VARCHAR(16) NOT NULL,
   expiration INT(64) NOT NULL,
   PRIMARY KEY (`token_hash`),
-  KEY username (client_id),
-  KEY target (username)
+  KEY client_id (client_id),
+  KEY username (username),
+  KEY expiration (expiration)
 );
 
 CREATE TABLE metadata (
   client_id VARCHAR(16) NOT NULL,
   username VARCHAR(16) NOT NULL,
-  metadata TEXT NOT NULL,
+  metadata LONGBLOB NOT NULL,
   PRIMARY KEY (`client_id`,`username`),
-  KEY username (client_id),
-  KEY target (username)
+  KEY client_id (client_id),
+  KEY username (username)
 );

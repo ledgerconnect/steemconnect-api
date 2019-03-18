20866705dc4fdc3b793ce72ba085ad0fcbbc9f25
adcpm <fabien@bonustrack.co>
2019-03-19 03:28:30 +0700

    Catch oauth2 promise errors

--- routes/oauth2.js
+++ routes/oauth2.js
@@ -17,7 +17,14 @@ router.all('/authorize', authenticate('user'), async (req, res) => {
     res.json({ code });
   } else {
     console.log(`Issue app token for user @${req.user} using @${clientId} proxy.`);
-    const accessToken = await issueAppToken(clientId, req.user, scope);
+
+    let accessToken;
+    try {
+      accessToken = await issueAppToken(clientId, req.user, scope);
+    } catch (e) {
+      console.error('Unable to issue app token on authorize', e);
+    }
+
     res.json({
       access_token: accessToken,
       expires_in: config.token_expiration,
@@ -29,7 +36,14 @@ router.all('/authorize', authenticate('user'), async (req, res) => {
 /** Request app access token */
 router.all('/token', authenticate(['code', 'refresh']), async (req, res) => {
   console.log(`Issue app token for user @${req.user} using @${req.proxy} proxy.`);
-  const accessToken = await issueAppToken(req.proxy, req.user, req.scope);
+
+  let accessToken;
+  try {
+    accessToken = await issueAppToken(req.proxy, req.user, req.scope);
+  } catch (e) {
+    console.error('Unable to issue app token', e);
+  }
+
   const payload = {
     access_token: accessToken,
     expires_in: config.token_expiration,
